6ca3f549a26b8691280091d32e728ec73555c1cd
junin__bh <portugalw@gmail.com>
2019-07-31 23:54:47 -0300

    enviando

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:todo/models/item.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TODO App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  var items = new List<Item>();
+
+  HomePage() {
+    items = [];
+    items.add(Item(title: "Add ela no Insta", done: true));
+    items.add(Item(title: "Pedir em namoro", done: true));
+    items.add(Item(title: "Noivar", done: true));
+    items.add(Item(title: "Casar com a branquinha", done: false));
+  }
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Text("Leading"),
+        title: Text("TODO List"),
+        actions: <Widget>[Icon(Icons.plus_one)],
+      ),
+      body: ListView.builder(
+        itemCount: widget.items.length,
+        itemBuilder: (BuildContext ctxt, int index) {
+          final item = widget.items[index];
+          return CheckboxListTile(
+            title: Text(item.title),
+            key: Key(item.title),
+            value: item.done,
+            onChanged: (key) {
+              print(key);
+            },
+          );
+        },
+      ),
+    );
+  }
+}
